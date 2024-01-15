dd8bc6926411559f63afabf4cf466fb33904e391
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2024-01-15 19:28:54 -0300

    Alterando carta carousel

--- components/viewer-card/viewer-card.js
+++ components/viewer-card/viewer-card.js
@@ -1,9 +1,25 @@
 function makeImageGrayscale(clickedImage) {
     var images = document.querySelectorAll('.carousel img');
-
+    
     images.forEach(function (image) {
-        image.classList.remove('grayscale');
+        image.classList.add('grayscale');
     });
 
-    clickedImage.classList.add('grayscale');
+    var isGrayscale = clickedImage.classList.contains('grayscale');
+
+    if (isGrayscale) {
+        clickedImage.classList.remove('grayscale');
+    } else {
+        clickedImage.classList.add('grayscale');
+    }
+}
+
+function changeImage(image){
+    var selectedCardImage = document.querySelector('.card-selected img');
+    selectedCardImage.src = image.src;
+}
+
+function selectImage(card) {
+    makeImageGrayscale(card);
+    changeImage(card);
 }
