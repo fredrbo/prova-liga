e6204833477ff9967e22ce82b3c740af5b31bd4a
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2024-01-16 16:21:01 -0300

    Estruturando cardName

---
+++ components/select/select.js
@@ -0,0 +1,8 @@
+var cardList = [
+    { id: 1, cardName: "Dominaria Remastered", numberCard: "045", edition: "Dominaria Remastered" },
+];
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+});
